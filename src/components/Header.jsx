85eccd79bcd9b14b985f885aca306de6a85ff306
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-02 14:22:33 -0700

    added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    // image placeholder?
+    return (
+        <>
+            <title>Food of Korea</title>
+            <nav>
+            </nav>
+        </>
+    
+    )
+}   
