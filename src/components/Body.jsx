1e820225b8539a31a954f3b8be0c2fbf03d48d97
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-02 14:39:43 -0700

    started body text

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,17 @@
 export default function Body() {
     return (
-        <p>Body Text</p>
+        <>
+            <h1>Theese are intended to generate from a data file using a component template but we do not have a file right now.</h1>
+            <h2>Ramen</h2>
+            <p>Description of Remen</p>
+
+            <h2>Corn Dogs</h2>
+            <p>Description of Corn Dogs</p>
+
+            <h2>Pizza</h2>
+            <p>Description of Pizza</p>
+
+            <h2></h2>
+        </>
     )
 }
