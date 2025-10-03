2fd88c6c9ea68fae69bbdfd22b71a41a4be3e084
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-10-02 17:02:42 -0700

    worked on the data asset

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,9 @@
+import foods from "../assets/data/foods.json";
+import Card from "./Card.jsx"
+
 export default function Body() {
     return (
-        <>
-            <h1>Theese are intended to generate from a data file using a component template but we do not have a file right now.</h1>
-            <h2>Ramen</h2>
-            <p>Description of Remen</p>
-
-            <h2>Corn Dogs</h2>
-            <p>Description of Corn Dogs</p>
-
-            <h2>Pizza</h2>
-            <p>Description of Pizza</p>
-
-            <h2></h2>
-        </>
+        <main>
+        </main>
     )
 }
